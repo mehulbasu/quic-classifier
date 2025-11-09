8d94e518db787f2f48536a939bd394f18a5f62c4
Mehul Basu <mehulbasu@gmail.com>
2025-11-09 17:19:37 -0600

    updated startup for conda

--- startup.sh
+++ startup.sh
@@ -9,11 +9,22 @@ git config --global user.email "mehulbasu@gmail.com"
 echo "Updating package list..."
 sudo apt update -y
 
-echo "Installing pip..."
-sudo apt install -y python3-pip
+# echo "Installing pip..."
+# sudo apt install -y python3-pip
 
-echo "Installing Python 3.10 venv package..."
-sudo apt install -y python3.10-venv
+# echo "Installing Python 3.10 venv package..."
+# sudo apt install -y python3.10-venv
+
+# Downloading Conda installer
+echo "Downloading Miniforge installer..."
+wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh -O Miniforge3-Linux-x86_64.sh
+
+# Installing Conda
+echo "Installing Miniforge..."
+bash Miniforge3-Linux-x86_64.sh -b -p $HOME/miniforge3
+
+# Remove installer
+rm Miniforge3-Linux-x86_64.sh
 
 echo "Installing Ubuntu drivers common package..."
 sudo apt install -y ubuntu-drivers-common
@@ -27,5 +38,5 @@ sudo apt install -y nvidia-cuda-toolkit
 # echo "Installing NVIDIA driver 580..."
 # sudo apt install -y nvidia-driver-580
 
-# echo "All installations complete. Rebooting system..."
-# sudo reboot
+echo "All installations complete. Rebooting system..."
+sudo reboot
