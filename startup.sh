aa94220f92bcf3d6102d355d79b7dce8e3110016
Mehul Basu <mehulbasu@gmail.com>
2025-11-09 18:01:20 -0600

    updated startup for cuda 12.8

--- startup.sh
+++ startup.sh
@@ -26,11 +26,21 @@ bash Miniforge3-Linux-x86_64.sh -b -p $HOME/miniforge3
 # Remove installer
 rm Miniforge3-Linux-x86_64.sh
 
+# Install CUDA
+echo "Downloading CUDA keyring package..."
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
+sudo apt-get -y install cuda-toolkit-12-8
+rm cuda-keyring_1.1-1_all.deb
+
+# Set up environment variables for CUDA
+echo "Setting up CUDA environment variables..."
+echo 'export PATH=/usr/local/cuda-12.8/bin${PATH:+:${PATH}}' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH=/usr/local/cuda-12.8/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}' >> ~/.bashrc
+source ~/.bashrc
+
 echo "Installing Ubuntu drivers common package..."
 sudo apt install -y ubuntu-drivers-common
-
-echo "Installing CUDA toolkit..."
-sudo apt install -y nvidia-cuda-toolkit
 
 # Run this command to list your GPU and the recommended driver
 # ubuntu-drivers devices
